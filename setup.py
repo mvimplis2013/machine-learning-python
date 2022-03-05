5dcf95c53d5d63345e607813452f870eebeb584f
miltos <mvimblis@gmail.com>
2022-03-05 18:15:57 +0200

    data_files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="0.0.1",
     packages=["camera4me"],
     install_requires=["opencv-python"],
-    data_files=[("camera_config", ["config/camera.cfg])],
+    data_files=[("camera_config", ["config/camera.cfg"])],
     entry_points={
       "console_scripts": [
         "smart-grab=camera4me.main:main_grab",
