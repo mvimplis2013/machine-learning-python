bad3abf3b46cbdeff1e92ced4180a0a7368784f7
miltos <mvimblis@gmail.com>
2022-03-05 16:31:45 +0200

    setup.py with entrypoints

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+from distutils.core import setup
+
 setup(
     name="capture_frames",
     version="0.0.1",
     packages=["camera4me"],
+    install_requires=["requests"],
+    py_modules=["cv2"],
+    entry_points={
+      "console_scripts": [
+        "smart-grab=camera4me.main:__main__",
+      ]
+    }
 )
