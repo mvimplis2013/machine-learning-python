1a48744cd8962905bfd6aeed28e96aecf24665b3
miltos <mvimblis@gmail.com>
2022-04-03 15:16:20 +0300

    folder size - running endless

--- camera4me/space_protector.py
+++ camera4me/space_protector.py
@@ -3,7 +3,10 @@ from os import listdir
 def count_frames(folder):
 	files_dirs = listdir(folder)
 
+	printf("Size of ....")
+
 def run_watchdog():
 	data_dir = "/dt2"
 
-	count_frames(data_dir)
+	while True:
+		count_frames(data_dir)
