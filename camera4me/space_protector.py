777df96fba3b91dcef5970b27c697dc752a2616d
miltos <mvimblis@gmail.com>
2022-04-05 16:39:36 +0300

    folder size - running endless

--- camera4me/space_protector.py
+++ camera4me/space_protector.py
@@ -1,14 +1,29 @@
-from os import listdir, chdir
+from os import listdir, chdir, remove
 from os.path import isfile, isdir, getmtime
 import time
 from datetime import datetime
 
 SLEEP_SECS = 10
 
+DAYS_OLD = 1    # One Day
+HOURS_OLD = 6   # Six Hours
+
+def older_than_days(days):
+	if days >= DAYS_OLD:
+		return True
+	else:
+		return False
+
+def older_than_hours(hours):
+	if hours >= HOURS_OLD:
+		return True
+	else:
+		return False
+
 def inside_date_folder(folder):
 	chdir(folder)
 	files = listdir('.')
-	print(f"Number of Frames inside Folder .... {len(files)} / {folder}")
+	#print(f"Number of Frames inside Folder .... {len(files)} / {folder}")
 
 	now = datetime.now()
 	
@@ -17,13 +32,18 @@ def inside_date_folder(folder):
 			then = datetime.fromtimestamp( getmtime(f) )
 			tdelta = now - then
 
-			#days    = tdelta.days
-			seconds = tdelta.total_seconds()
+			days    = tdelta.days
+			seconds = tdelta.seconds
 			minutes = seconds / 60.0
 			hours   = minutes / 60.0
-			days    = hours / 24
+			#days    = hours / 24
 
-			print(f"Total Seconds Since File Modification Time .... {f} / {seconds}secs / {minutes}mins / {hours}hrs / {days}days")
+			if older_than_days(days):
+				print(f"Ready to Delete Frame More Than Day(s) Old .... {f} / {days}-(days)")
+				remove(f)
+			elif older_than_hours(hours):
+				print(f"Ready to Delete Frame More Than Hours(s) Old .... {f} / {hours}-(hrs)")
+				remove(f)
 			
 def count_frames(folder):
 	try:
@@ -33,7 +53,7 @@ def count_frames(folder):
 		print(f"Cannot change into FRAMES folder ... {x}")
 		exit(1)
 
-	print(f"Folders inside Frames = {files_dirs}")
+	#print(f"Folders inside Frames = {files_dirs}")
 	
 	for f in files_dirs:
 		if isdir(f):
