9016d17f6cc11295e4637718b01ce898cd3588bd
miltos <mvimblis@gmail.com>
2022-04-05 12:09:49 +0300

    folder size - running endless

--- camera4me/space_protector.py
+++ camera4me/space_protector.py
@@ -1,25 +1,26 @@
-from os import listdir, chdir
+from os import listdir, chdir, isfile, isdir
 import time
 
 SLEEP_SECS = 10
 
+def inside_date_folder(folder):
+	chdir(folder)
+	files_dirs = listdir('.')
+	print(f"Number of Frames .... {len(files_dirs)}")
+
 def count_frames(folder):
 	try:
-		files_dirs = listdir(folder)
+		chdir(folder)
+		files_dirs = listdir(".")
 	except OSError as x:
 		print(f"Cannot change into FRAMES folder ... {x}")
 		exit(1)
 
 	print(f"Folders inside Frames = {files_dirs}")
-
-	# try:
-	# 	chdir(files_dirs)
-	# except OSError as x:
-	# 	print(f"Cannot change into DATE folder ... {x}")
-	# 	exit(1)
 	
-	# files_dirs = listdir('.')
-	# print(f"Number of Frames .... {len(files_dirs)}")
+	for f in files_dirs:
+		if isdir(f):
+			inside_date_folder(f)
 
 def run_watchdog():
 	data_dir = "/dt2/video/frames/"
