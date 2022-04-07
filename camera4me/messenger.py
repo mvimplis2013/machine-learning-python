7e7451fc3b3608a4fd9bed39057d383e31a4420c
miltos <mvimblis@gmail.com>
2022-04-07 12:25:16 +0300

    folder size - running endless

--- camera4me/messenger.py
+++ camera4me/messenger.py
@@ -5,7 +5,7 @@ def __main_mq_client__():
 
 	parser = argparse.ArgumentParser( description='Handle Messages Between MicroServices' )
 
-	parser.add_argument( required=False, choices=['sent', 'receive', 'delete'], help='action for backend message-queue service', default='sent' )
+	parser.add_argument( 'action', required=False, choices=['sent', 'receive', 'delete'], help='action for backend message-queue service', default='sent' )
 
 	parser.add_argument( '-n', '--name', required=False, type=str, help='message-queue name', default="Message-Queue-VIBM" )
 	parser.add_argument( '-p', '--port', required=False, type=int, help='listen-port', default=5672 )
@@ -13,7 +13,7 @@ def __main_mq_client__():
 	args = parser.parse_args()
 
 	if args.action == "sent":
-	  print( f"SEND-Action {args.name}, args.port" )
+	  print( f"SEND-Action {args.name}, {args.port}" )
 
 	'''
 	elif args.action == "receive":
