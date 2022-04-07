9ab0286bf097950f86e4c5d00cd6e40b57f9b0ed
miltos <mvimblis@gmail.com>
2022-04-07 12:08:14 +0300

    folder size - running endless

--- camera4me/messenger.py
+++ camera4me/messenger.py
@@ -7,8 +7,8 @@ def __main_mq_client__():
 
 	parser.add_argument( '-a', '--action', required=False, choices=['sent', 'receive', 'delete'], help='action for backend message-queue service' )
 
-	#parser.add_argument( '-n', '--name', required=False, type=str, help='message-queue name', default="Message-Queue-VIBM" )
-	#parser.add_argument( '-p', '--port', required=False, type=int, help='listen-port', default=5672 )
+	parser.add_argument( '-n', '--name', required=False, type=str, help='message-queue name', default="Message-Queue-VIBM" )
+	parser.add_argument( '-p', '--port', required=False, type=int, help='listen-port', default=5672 )
 
 	args = parser.parse_args()
 
