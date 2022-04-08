e903e2a8f6fc51b36cf94fe7e524043ee197de15
miltos <mvimblis@gmail.com>
2022-04-08 15:57:50 +0300

    folder size - running endless

--- camera4me/influxdb_main.py
+++ camera4me/influxdb_main.py
@@ -1,6 +1,15 @@
 from influxdb import InfluxDBClient
 
+import os
+
 def __influx_main__():
 	print(f"Ready to Connect to InfluxDB")
+
+    host = os.environ[ "INFLUXDB_SERVICE" ]
+    password = os.environ[ "INFLUXDB_PASSWORD" ]
+
+	client = InfluxDBClient( host, user="admin", password )
+
+	client.close()
 
 	return
