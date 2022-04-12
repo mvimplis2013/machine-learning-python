87f067386eeed4af9865de0ac7d19badb0407518
miltos <mvimblis@gmail.com>
2022-04-12 16:54:12 +0300

    folder size - running endless

--- camera4me/influxdb_main.py
+++ camera4me/influxdb_main.py
@@ -8,13 +8,13 @@ def write_events():
 def __influx_main__():
 	print(f"Ready to Connect to InfluxDB")
 
-	host = os.environ[ "INFLUXDB_SERVICE" ]
-	print(f"Host = {host}")
+	#host = os.environ[ "INFLUXDB_SERVICE" ]
+	#print(f"Host = {host}")
 
-	username = os.environ[ "INFLUXDB_USER" ]
-	password = os.environ[ "INFLUXDB_PASSWORD" ]
+	#username = os.environ[ "INFLUXDB_USER" ]
+	#password = os.environ[ "INFLUXDB_PASSWORD" ]
 	
-	database = os.environ[ "INFLUXDB_NAME" ]
+	#database = os.environ[ "INFLUXDB_NAME" ]
 	
 	try:
 		client = InfluxDBClient( host="vibm-influxdb-influxdb2", port=8086, username="admin", password="mnzLrGbCpH89okUbSzpLHuPKC8iFXbXJ" )
