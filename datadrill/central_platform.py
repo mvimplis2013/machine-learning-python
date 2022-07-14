d2b583564c2037c28237303d92f2a30f02b0bcc8
miltos <mvimblis@gmail.com>
2022-07-14 16:20:27 +0300

    get_disk_usage()

--- datadrill/central_platform.py
+++ datadrill/central_platform.py
@@ -1,4 +1,11 @@
 import shutil
 
+KB = 1024
+MB = 1024 * KB
+GB = 1024 * MB
+
+def get_disk_usage():
+	return shutil.disk_usage('/').free / GB
+
 def platform_22():
-	print(f"Ready to start system monitoring!!!{shutil.__version__}")
+	print(f"Ready to start system monitoring ... Free Disk Space = {get_disk_usage()}")
