0a1e1ed4c87cde4dbcd4fbdb0723c4615af58780
miltos <mvimblis@gmail.com>
2022-09-22 13:35:38 +0300

    Trying to Ping Redis

--- camera4me/messenger.py
+++ camera4me/messenger.py
@@ -62,7 +62,7 @@ def call_redis_to_communicate( host, port ):
 	print( f"Using Redis to Send Messages ... Python-Client ver. {redis.__version__}" )
 
 	r = redis.Redis(host=host, port=port)
-	r.ping()
+	assert r.ping()
 
 
 def __main_mq_client__():
