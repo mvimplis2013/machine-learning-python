53fb7965983688036927475fbf5bb27455c0c4cb
miltos <mvimblis@gmail.com>
2023-02-02 12:18:54 +0200

    Added logging code into basic_consumer for messageKit/rabbitmq

--- messageKit/rabbitmq/consume.py
+++ messageKit/rabbitmq/consume.py
@@ -7,12 +7,12 @@ import logging
 import pika
 from pika.exchange_type import ExchangeType
 
-LOG_FORMAT = (("%(levelname)"))
+LOG_FORMAT = (("%(levelname) -10s %(asctime)s %(name) -30s"))
 LOGGER = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
- 
+
 def main():
 	"""Main Method"""
 
