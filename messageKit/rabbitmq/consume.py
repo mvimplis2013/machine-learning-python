4d08009db2fab3b324347b097c3e76bce817ef31
miltos <mvimblis@gmail.com>
2023-02-02 13:25:55 +0200

    Added ArgsParsing Host/ Port code into basic_consumer for messageKit/rabbitmq

--- messageKit/rabbitmq/consume.py
+++ messageKit/rabbitmq/consume.py
@@ -23,10 +23,13 @@ def main():
 
 	parser = argparse.ArgumentParser( description='Arguments for RabbitMQ Basic Message Consumer' )
 
+	# Credentials
 	parser.add_argument( '-u', '--user', required=True, type=str, help='Username for RabbitMQ Connection', default='tandem' )
 	parser.add_argument( '-p', '--password', required=True, type=str, help='Password for RabbitMQ Connection' )
 	
-	#parser.add_argument( '-p', '--port', required=False, type=int, help='listen-port', default=5672 )
+	# Location
+	parser.add_argument( '-h', '--host', required=True, type=str, help='RabbitMQ Server IP-Address')
+	parser.add_argument( '-p', '--port', required=False, type=int, help='RabbitMQ Server AMQP-Port', default=5672 )
 
 	args = parser.parse_args()
 
