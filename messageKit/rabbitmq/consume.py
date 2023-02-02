41d1737c39caeb8dca8b1be96af4d228f6b6078d
miltos <mvimblis@gmail.com>
2023-02-02 12:12:02 +0200

    Added code into basic_consumer for messageKit/rabbitmq

--- messageKit/rabbitmq/consume.py
+++ messageKit/rabbitmq/consume.py
@@ -2,6 +2,17 @@
 Basic Message Consumer Example
 """
 
+import functools
+import logging
+import pika
+from pika.exchange_type import ExchangeType
+
+LOG_FORMAT = (("%(levelname)"))
+LOGGER = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+ 
 def main():
 	"""Main Method"""
 
