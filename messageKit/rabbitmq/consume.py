c8569733fa2b6aed9787b79e37bfdd4f7f0c07b8
miltos <mvimblis@gmail.com>
2023-02-02 13:10:40 +0200

    Added ArgsParsing code into basic_consumer for messageKit/rabbitmq

--- messageKit/rabbitmq/consume.py
+++ messageKit/rabbitmq/consume.py
@@ -1,6 +1,7 @@
 """
 Basic Message Consumer Example
 """
+import argparse
 
 import functools
 import logging
@@ -20,5 +21,15 @@ def main():
 	print("*** Basic Message Consumer for RabbitMQ ***")
 	print("*******************************************")
 
-	LOGGER.info("Delivery properties: %s", "@DUMMY@")
+	parser = argparse.ArgumentParser( description='Arguments for RabbitMQ Basic Message Consumer' )
+
+	parser.add_argument( '-u', '--user', required=True, type=str, help='Username for RabbitMQ Connection', default='tandem' )
+	parser.add_argument( '-p', '--password', required=True, type=str, help='Password for RabbitMQ Connection' )
+	
+	#parser.add_argument( '-p', '--port', required=False, type=int, help='listen-port', default=5672 )
+
+	args = parser.parse_args()
+
+	LOGGER.info("Delivery properties: %s", args.user)
+	
 	return
