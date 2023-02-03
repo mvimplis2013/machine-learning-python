29e35b28cbf6eb7f6590e0f9c55d3182fdd9b7dd
miltos <mvimblis@gmail.com>
2023-02-03 16:06:31 +0200

    Added ArgParser in RabbitMQ-ASYNCHRONOUS-CONSUMER

--- messageKit/rabbitmq/asynchronous_consumer.py
+++ messageKit/rabbitmq/asynchronous_consumer.py
@@ -1,7 +1,7 @@
 import logging
 import argparse
 
-LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(function) -35s %(lineno) -5d: %(message)s')
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lineno) -5d: %(message)s')
 LOGGER = logging.getLogger(__name__)
 
 class ExampleConsumer(object):
