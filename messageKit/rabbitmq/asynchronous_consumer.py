6eab2855b0e908de3e7cf4c8857628c24a96d88e
miltos <mvimblis@gmail.com>
2023-02-06 14:00:10 +0200

    Importing Required Modules into ExampleConsumer

--- messageKit/rabbitmq/asynchronous_consumer.py
+++ messageKit/rabbitmq/asynchronous_consumer.py
@@ -1,6 +1,9 @@
 import logging
 import argparse
 
+import pika
+from pika.exchange_type import ExchangeType
+
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lineno) -5d: %(message)s')
 LOGGER = logging.getLogger(__name__)
 
