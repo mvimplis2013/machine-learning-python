045d1df030081c910f783fbc96fd7eb74935363b
miltos <mvimblis@gmail.com>
2023-02-06 13:32:58 +0200

    Implementing ExampleConsumer that connects to RabbitMQ and manages the connection

--- messageKit/rabbitmq/asynchronous_consumer.py
+++ messageKit/rabbitmq/asynchronous_consumer.py
@@ -5,14 +5,59 @@ LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lin
 LOGGER = logging.getLogger(__name__)
 
 class ExampleConsumer(object):
-	def __init__(self):
+	"""
+	This is an example consumer that will handle unexpected interactions with RabbitMQ such as channel and
+	connection closure.
+
+	If RabbitMQ closes the connection, thi class will stop and indicate that reconnection is necessary. 
+	You should look at the output, as there are limited reasons why the connection may be closed.
+	"""
+	EXCHANGE = "message"
+	EXCHANGE_TYPE=ExchangeType.topic
+	QUEUE = 'text'
+	ROUTING_KEY = 'example.text'
+
+	def __init__(self, amqp_url):
+		"""
+		Create a new instance of the consumer class, passing it the AMQP URL used to connect to RabbitMQ.
+		
+		:param str amqp_url: The AMQP URL to connect with RabbitMQ
+		"""
+        self.should_reconnect = False
+        self.was_consuming = False 
+
+        self._connection = None
+        self._channel = None
+
+        self._closing = False
+        self._consumer_tag = None
+        self._consuming = False 
+		self._url = amqp_url
+
+		# In Production, experiment with higher prefetch values for higher consumer throughput
+		self._prefetch_count = 1
+
 		return
 
+	def connect(self):
+		"""
+		This method connects to RabbitMQ, returning the connection handle.
+		When the connection is established, the on_connection_open method will be invoked by pika.
+
+		rtype: pika.SelectConnection
+
+		LOGGER.info("Connection to %s", self._url)
+
+		return pika.SelectConnection(
+		)
+
 class ReconnectingExampleConsumer(object):
-	def __init__(self):
+	def __init__(self, amqp_url):
 		print("This is a consumer that will try to reconnect if nested connection is lost")
 
-		self._consumer = ExampleConsumer()
+		self._amqp_url = amqp_url
+
+		self._consumer = ExampleConsumer(self._amqp_url)
 
 		return
 
@@ -55,7 +100,7 @@ def asynchronous_consumer_main():
 	LOGGER.info(amqp_url)
 	#print( args.user )
 	
-	example_consumer = ReconnectingExampleConsumer()
+	example_consumer = ReconnectingExampleConsumer(amqp_url)
 	example_consumer.run()
 
 # ******  ENTRYPOINT ******
