91d08a337b8b3fa152d926c0a9efa3f4d44ced3b
miltos <mvimblis@gmail.com>
2023-02-07 14:57:36 +0200

    Added ASYNC-PUBLISHER-RABMQ in messaging-operator

--- messageKit/rabbitmq/asynchronous_publisher.py
+++ messageKit/rabbitmq/asynchronous_publisher.py
@@ -1,5 +1,10 @@
 import logging
 
+LOG_FORMAT = (%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lineno) -5d: %(message)s)
+LOGGER = logging.getLogger(__name__)
+
 def main():
 	logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+
+	logging.INFO("Ready to Start the ASYNCHRONOUS_PUBLISHER to RabbitMQ !")
 
