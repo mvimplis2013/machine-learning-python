b9c068dd1460265e4f6e2c278766968b471f654e
miltos <mvimblis@gmail.com>
2023-02-07 15:48:39 +0200

    Added ASYNC-PUBLISHER-RABMQ in messaging-operator

--- messageKit/rabbitmq/asynchronous_publisher.py
+++ messageKit/rabbitmq/asynchronous_publisher.py
@@ -1,5 +1,7 @@
 import logging
 
+from pika.exchange_type import ExchangeType
+
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lineno) -5d: %(message)s')
 LOGGER = logging.getLogger(__name__)
 
