ba3ab390c8241099f7d83d6867563ca2216e4778
miltos <mvimblis@gmail.com>
2023-02-09 13:43:18 +0200

    Add Publish_Message() in ExamplePublisher

--- messageKit/rabbitmq/asynchronous_publisher.py
+++ messageKit/rabbitmq/asynchronous_publisher.py
@@ -332,7 +332,7 @@ class ExamplePublisher(object):
 
 		self._message_number += 1
 
-		self.deliveries[self._message_number] = capture_frames
+		self.deliveries[self._message_number] = True 
 
 		LOGGER.info(
 			"Published Message # %i", self._message_number)
