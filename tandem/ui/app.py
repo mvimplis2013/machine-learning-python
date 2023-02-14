9c29d73dac5faaa118719fcaaa6da5ea5e70dc89
miltos <mvimblis@gmail.com>
2023-02-14 10:55:46 +0200

    Install flask-application in python/wheel

--- tandem/ui/app.py
+++ tandem/ui/app.py
@@ -1,12 +1,22 @@
-import os
+import click
 from flask import Flask
 
 app = Flask(__name__) 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-	return "<h1>Hello, worl</h1>"
+	return "<h1>Hello, world !</h1>"
 
+@app.route("/hi")
+@app.route("/hello")
+def say_hello():
+	return "<h1>Hello, flask !</h1>"
+
+# Dynamic Route
+@app.route("/greet", defaults={"name": "Pete"})
+def greet(name):
+	return "<h1>Hello, %s !</h1>" %name 
+	
 @app.cli.command()
 def main_flask():
 	click.echo("Hello, Miltos!")
