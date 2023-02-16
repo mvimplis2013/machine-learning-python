fdbf0ebe8f19309d7c29b8a4f1a965100d4d2ede
miltos <mvimblis@gmail.com>
2023-02-16 16:04:32 +0200

    Frames-to-Video Converter

--- setup-video.py
+++ setup-video.py
@@ -7,17 +7,9 @@ setup(
     version="0.0.1",
     packages=["camera4me"],
     install_requires=["opencv-python"],
-    data_files=[("cfg", ["config/camera.cfg"])],
+    data_files=[],
     entry_points={
       "console_scripts": [
-        "smart-grab=camera4me.main:main_grab",
-        "file-watchdog=camera4me.space_protector:run_watchdog",
-        "polite-messenger=camera4me.messenger:__main_mq_client__",
-        "influxdb-caller=camera4me.influxdb_main:__influx_main__",
-        "system-monitoring=datadrill.central_platform:platform_22",
-        "rabbitmq-consumer=messageKit.rabbitmq.consume:main",
-        "async-consumer-rabmq=messageKit.rabbitmq.asynchronous_consumer:asynchronous_consumer_main",
-        "async-publisher-rabmq=messageKit.rabbitmq.asynchronous_publisher:main",
         "video-creator=camera4me.frames_to_video:video_main"
       ]
     }
