6e1f5a103c69a60d7af404e7cdb38d7d54b7da52
miltos <mvimblis@gmail.com>
2023-02-17 15:58:19 +0200

    CMD : python3 setup-video && ENTRYPONT: video-creator

--- camera4me/frames_to_video.py
+++ camera4me/frames_to_video.py
@@ -10,20 +10,20 @@ VIDEO_FOLDER = "/data/video/"
 
 img_array = []
 
-for filename in glob.glob('C:/users/vibm/Downloads/rainy/2021-10-14-09_55_51/*.jpg'):
-    img = cv.imread(filename)
-    height, width, layers = img.shape
+# for filename in glob.glob('C:/users/vibm/Downloads/rainy/2021-10-14-09_55_51/*.jpg'):
+#     img = cv.imread(filename)
+#     height, width, layers = img.shape
 
-    size = (width, height)
+#     size = (width, height)
 
-    img_array.append(img)
+#     img_array.append(img)
 
-out = cv.VideoWriter('project.avi', cv.VideoWriter_fourcc(*'DIVX'), 15, size)
+# out = cv.VideoWriter('project.avi', cv.VideoWriter_fourcc(*'DIVX'), 15, size)
 
-for i in range(len(img_array)):
-    out.write(img_array[i])
+# for i in range(len(img_array)):
+#     out.write(img_array[i])
 
-out.release()
+# out.release()
 
 def check_if_directory_exists( p ):
     #os.path.exists()
