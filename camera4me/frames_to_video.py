4ab860c010265d7aedb6950fc07212fb584b1168
miltos <mvimblis@gmail.com>
2023-02-23 16:33:43 +0200

    Convert AVI video to MP4

--- camera4me/frames_to_video.py
+++ camera4me/frames_to_video.py
@@ -17,7 +17,7 @@ VIDEO_FOLDER = "/data/frames/"        # Store video on parent-folder
 # VIDEO CONVERSION
 VIDEO_FILE_NAME = "/parking-space-masked.avi"
 MP4_NAME = "/parking-space-masked.mp4"
-FFMPEG_COMMAND = f"ffmpeg -i {VIDEO_FILE_NAME} -ac 2 -b:v 2000k -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 {MP4_NAME}"
+FFMPEG_COMMAND = f"ffmpeg -i {VIDEO_FOLDER + VIDEO_FILE_NAME} -ac 2 -b:v 2000k -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 {MP4_NAME}"
 
 # for filename in glob.glob('C:/users/vibm/Downloads/rainy/2021-10-14-09_55_51/*.jpg'):
 #     img = cv.imread(filename)
