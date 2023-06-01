53b308a2cc956b9dd4ca721a0a1093a2e4254b02
maxez <maxez@intracom-telecom.com>
2023-06-01 15:03:39 +0300

    log level = INFO

--- camera4me/messenger.py
+++ camera4me/messenger.py
@@ -9,7 +9,7 @@ import redis
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 def call_mq_server( host, password ):
 	print(f"Ready to Start Sending Messages with RabbitMQ")
